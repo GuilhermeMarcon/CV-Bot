0a97305c0f2e95ddc0060eab404f4467f24d0321
GuilhermeMarcon <guismarcon@hotmail.com>
2024-11-25 15:21:01 -0300

    update back end url

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -16,7 +16,7 @@ const Chatbot = () => {
 
     try {
       // Send message to back-end
-      const response = await axios.post("http://localhost:8000/chat", {
+      const response = await axios.post("https://cv-bot-back.onrender.com/chat", {
         message: userInput,
       });
 
